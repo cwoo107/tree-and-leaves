441b702fc5ac7d4b21ac3c080cb70d62e7aef8dd
Caleb Woods <woodscaleb@gmail.com>
2020-12-09 12:24:31 -0800

    setup sendgrid support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'treeandleaves.com',
+      :domain => 'em9516.treeandleaves.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
