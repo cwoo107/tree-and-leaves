c42c488e2a023c5e0a69b067192cc677196217d0
Caleb Woods <woodscaleb@gmail.com>
2020-12-09 13:44:16 -0800

    added edit and delete buttons

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,6 +2,6 @@ class Folder < ApplicationRecord
   belongs_to :user
   belongs_to :folderable, polymorphic: true, optional: true
   accepts_nested_attributes_for :folderable
-  has_many :folders, as: :folderable
-  has_many :notes, as: :folderable
+  has_many :folders, as: :folderable, :dependent => :delete_all
+  has_many :notes, as: :folderable, :dependent => :delete_all
 end
